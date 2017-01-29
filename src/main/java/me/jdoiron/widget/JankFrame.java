139058aef16776a5f26dc4e7e3aa1bf857f51871
Jacob <jdoiron@smcm.edu>
2017-01-28 23:58:27 -0500

    Add data entry support

--- src/main/java/me/jdoiron/widget/JankFrame.java
+++ src/main/java/me/jdoiron/widget/JankFrame.java
@@ -2,18 +2,29 @@ package me.jdoiron.widget;
 
 import javafx.application.Application;
 import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import me.jdoiron.widget.menu.MenuBar;
 import me.jdoiron.widget.table.EditCell;
 import me.jdoiron.widget.table.Row;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.function.Function;
 
@@ -74,11 +85,11 @@ public class JankFrame extends Application {
                 columnBalance);
 
         LocalDate localDate = LocalDate.now();
-        String date = String.format("%02d/%02d/%04d", localDate.getMonthValue(), localDate.getDayOfMonth(),
+        String dateToday = String.format("%02d/%02d/%04d", localDate.getMonthValue(), localDate.getDayOfMonth(),
                 localDate.getYear());
 
         table.getItems().addAll(
-                new Row("1", "X", date, "Initial balance", "5.00", "",
+                new Row("1", "X", dateToday, "Initial balance", "5.00", "",
                         "5.00")
         );
 
@@ -87,8 +98,14 @@ public class JankFrame extends Application {
             TablePosition<Row, String> pos = table.getFocusModel().getFocusedCell();
             if (pos != null && StringUtils.isNotEmpty(text) &&
                     (StringUtils.isAlphanumericSpace(text) || StringUtils.isAsciiPrintable(text))) {
-                System.out.println(event);
                 table.edit(pos.getRow(), pos.getTableColumn());
+            }
+        });
+
+        table.setOnKeyReleased(e -> {
+            TablePosition<Row, String> cell = getSelectedCell(table);
+            if (cell != null && cell.getTableColumn().getText().equals("Row")) {
+                System.out.println(cell.getRow());
             }
         });
 
@@ -97,9 +114,67 @@ public class JankFrame extends Application {
         scrollTable.setFitToWidth(true);
         scrollTable.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
 
-        vbox.getChildren().addAll(new MenuBar(table, primaryStage).bar(), scrollTable);
+        CheckBox boxConfirm = new CheckBox("Confirm");
+
+        DatePicker fieldDate = new DatePicker();
+        fieldDate.setPromptText("Date");
+        TextField fieldDesc = new TextField();
+        fieldDesc.setPromptText("Description");
+        TextField fieldCredit = new TextField();
+        fieldCredit.setPromptText("Credit");
+        TextField fieldDebit = new TextField();
+        fieldDebit.setPromptText("Debit");
+
+        Button buttonSubmit = new Button("Submit");
+        buttonSubmit.setOnAction(e -> {
+            String row = Integer.toString(table.getItems().size() + 1);
+            String confirm = boxConfirm.isSelected() ? "X" : "";
+            String date = fieldDate.getEditor().getText();
+            String description = fieldDesc.getText();
+            String credit = fieldCredit.getText().replace(',', '.');
+            String debit = fieldDebit.getText().replace(',', '.');
+            if (StringUtils.isNotBlank(date) && StringUtils.isNotBlank(description) && (NumberUtils.isParsable(credit) || NumberUtils.isParsable(debit))) {
+                String prevBalance = table.getItems().get(table.getItems().size() - 1).getBalance();
+                double newCredit = StringUtils.isBlank(credit) || !NumberUtils.isParsable(credit) ? 0.0 : Double.parseDouble(credit);
+                double newDebit = StringUtils.isBlank(debit) || !NumberUtils.isParsable(debit) ? 0.0 : Double.parseDouble(debit);
+                String formattedCredit = StringUtils.isBlank(credit) || !NumberUtils.isParsable(credit) ? "" : String.format("%.02f", Double.parseDouble(credit));
+                String formattedDebit = StringUtils.isBlank(debit) || !NumberUtils.isParsable(debit) ? "" : String.format("%.02f", Double.parseDouble(debit));
+                String newBalance = String.format("%.02f", Double.parseDouble(prevBalance) + newCredit - newDebit);
+                table.getItems().add(
+                        new Row(row, confirm, date, description, formattedCredit, formattedDebit, newBalance)
+                );
+                boxConfirm.setSelected(false);
+                fieldDate.getEditor().setText(null);
+                fieldDesc.setText(null);
+                fieldCredit.setText(null);
+                fieldDebit.setText(null);
+            }
+        });
+
+        HBox hbox = new HBox(20);
+        hbox.setPadding(new Insets(20, 0, 0, 20));
+        hbox.getChildren().addAll(boxConfirm, fieldDate, fieldDesc, fieldCredit, fieldDebit, buttonSubmit);
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.add(hbox, 0, 1, 1, 1);
+
+        vbox.getChildren().addAll(new MenuBar(table, primaryStage).bar(), scrollTable, gridPane);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(t -> System.out.println("closing"));
+    }
+
+    /**
+     * Returns the actively selected cell.
+     *
+     * @param table The TableView representing the table
+     * @return The selected cell, should it exist
+     */
+    private TablePosition<Row, String> getSelectedCell(TableView<Row> table) {
+        ObservableList<TablePosition> selectedCells = table.getSelectionModel().getSelectedCells();
+        return selectedCells != null && selectedCells.size() > 0 ? selectedCells.get(0) : null;
     }
 }
